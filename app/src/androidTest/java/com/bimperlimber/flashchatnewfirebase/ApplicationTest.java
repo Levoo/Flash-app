027f465b8a0c2fbbbdb2c6cbad8abc0b8e0faed7
white rose <fzavala649@gmail.com>
2017-07-13 19:26:49 -0700

    workin on it vid 132 4:30

--- app/src/androidTest/java/com/bimperlimber/flashchatnewfirebase/ApplicationTest.java
+++ app/src/androidTest/java/com/bimperlimber/flashchatnewfirebase/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.bimperlimber.flashchatnewfirebase;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/test/java/com/bimperlimber/flashchatnewfirebase/ExampleUnitTest.java
+++ app/src/test/java/com/bimperlimber/flashchatnewfirebase/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.londonappbrewery.flashchatnewfirebase;
+package com.bimperlimber.flashchatnewfirebase;
 
 import org.junit.Test;
 
